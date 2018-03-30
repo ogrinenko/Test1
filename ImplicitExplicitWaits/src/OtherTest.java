6a8f139d93475b030da3c14b44539de8f7482009
ImMortal <ImMortal@ImMortal-PC>
2018-03-30 17:34:53 +0300

    Adding Suite Runner

--- ImplicitExplicitWaits/src/OtherTest.java
+++ ImplicitExplicitWaits/src/OtherTest.java
@@ -1,3 +1,6 @@
+import java.util.Arrays;
+import java.util.Collection;
+
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -5,14 +8,28 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
-//@RunWith(Parameterized.class)
-
+@RunWith(Parameterized.class)
 public class OtherTest {
 
+	@Parameters
+	public static Collection<Object[]> getParams () {
+		return Arrays.asList(new Object[][] {{0,0},{1,1}});
+	}
+	
+	private int firstArg;
+	private int secondArg;
+	
+	public OtherTest(int first, int second) {
+		this.firstArg = first;
+		this.secondArg = second;
+	}
+	
+	
 	@BeforeClass
 	public static void otherBeforeClass() {
-		System.out.println("Other Before Class");
+		System.out.println("Other Before Class" );
 	}
 	
 	@AfterClass
@@ -23,23 +40,23 @@ public class OtherTest {
 	
 	@Before
 	public void setup() {
-		System.out.println("Other Before");
+		System.out.println("Other Before " + firstArg + " " + secondArg);
 	}
 	
 	
 	@Test
 	public void test1() {
-		System.out.println("Other test1");
+		System.out.println("Other test1 "  + firstArg + " " + secondArg);
 	}
 	
 	@Test
 	public void test2() {
-		System.out.println("Other test2");
+		System.out.println("Other test2 "  + firstArg + " " + secondArg);
 	}
 	
 	@After
 	public void cleanup() {
-		System.out.println("Other After");
+		System.out.println("Other After "  + firstArg + " " + secondArg);
 	}
 	
 }
---
+++ ImplicitExplicitWaits/src/TestSuiteClass.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+public class TestSuiteClass {
+
+	@Test
+	public void main() {
+		System.out.println("Test Suite Class");
+	}
+	
+}
