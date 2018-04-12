ad93e069c4338205ffa3d2a20b951bbd6ce8e51f
ImMortal <ImMortal@ImMortal-PC>
2018-04-12 13:28:20 +0300

    Test1

--- ImplicitExplicitWaits/src/TestMail.java
+++ ImplicitExplicitWaits/src/TestMail.java
@@ -1,4 +1,6 @@
+import java.io.File;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import org.junit.After;
 import org.junit.AfterClass;
@@ -8,6 +10,13 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite.SuiteClasses;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.By.ByXPath;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Suite;
 
@@ -16,9 +25,18 @@ import org.junit.runners.Suite;
 @RunWith(Suite.class)
 public class TestMail {
 
+	static WebDriver driver;
+	
+	
 	@BeforeClass
 	public static void setup() {
 		System.out.println("Test Before Class");
+		System.setProperty("webdriver.chrome.driver", new File("chromedriver.exe").getAbsolutePath());
+		driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
+		WebElement element = (new WebDriverWait(driver, 10).until(ExpectedConditions.presenceOfElementLocated(By.xpath(("//")))));
+		
 	}
 	
 		
@@ -29,5 +47,8 @@ public class TestMail {
 	
 	private List<String> stringList;
 	private List<Integer> integerList;
+
+	//Add sting comment
+	
 	
 }
